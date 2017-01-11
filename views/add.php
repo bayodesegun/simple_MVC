0eed950dc2aaa6f31c3725b17d0a4356c4839864
Bayode Aderinola <bayodesegun@gmail.com>
2017-01-11 19:48:17 +0100

    Fine-tune list-add page

--- views/add.php
+++ views/add.php
@@ -2,17 +2,16 @@
 
 <div class="container">
 	<h4>Add a Contact to database.</h4>
-	<p><span class="text-danger">* required field.</span></p>
+	<p><span class="text-danger">* required field.</span></p>  	
 	<form method="POST" id="add" name="add" action ="/add"> 
-		<b>Name:</b> <input type="text" name="name" size="55" value="<?php echo $name;?>" required> <span class="text-danger">* </span>
+		<b>Name:</b> <input type="text" name="name" size="100%" value="<?php echo $name;?>" required> <span class="text-danger">* </span>
 		<p class="text-danger"> <?php echo $nameError;?> </p>
 		<br> 
-		<b>Email:</b> <input type="text" name="email" size="55" value="<?php echo $email;?>" required> <span class="text-danger">*</span>
+		<b>Email:</b> <input type="text" name="email" size="100%" value="<?php echo $email;?>" required> <span class="text-danger">*</span>
 		<p class="text-danger"> <?php echo $emailError;?> </p>
-		<br>
 		<p class="text-success lead"> <?php echo $addSuccess;?> </p>
 		<br>		
-		 <input class="btn btn-success pull-right" type="submit" name="submit" value= "Submit"> 
+		 <input class="btn btn-success" type="submit" name="submit" value= "Submit"> 
 	</form>
 
 </div>
